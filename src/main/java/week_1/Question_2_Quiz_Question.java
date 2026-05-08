077778393f7a867fc8f7da18bb4ee722d24352b3
Corin Dennison <corin.dennison@gmail.com>
2026-05-08 12:45:47 -0500

    Update Question_2_Quiz_Question.java

--- src/main/java/week_1/Question_2_Quiz_Question.java
+++ src/main/java/week_1/Question_2_Quiz_Question.java
@@ -2,29 +2,27 @@ package week_1;
 
 import static input.InputUtils.stringInput;
 
-/**
- *
- * Finish the method that checks if the answer to the quiz question is correct.
- *
- * You should accept answers in any case, but the spelling and spacing must be correct.
- *
- * (Tip: the answer is "Pong" https://en.wikipedia.org/wiki/Pong)
- *
- */
 
 public class Question_2_Quiz_Question {
     
     public static void main(String[] args) {
         
+        // Print the quiz question to the user
         System.out.println("Quiz time!");
         System.out.println("What is the name of the classic 1972 arcade game based on table tennis?");
+        
+        // Get the user's answer as a String
         String answer = stringInput("Enter your answer: ");
         
+        // Call method to check if the answer is correct (true or false)
         boolean correct = checkAnswer(answer);
         
+        // If correct is true → print correct message
         if (correct) {
             System.out.println("You are correct!");
-        } else {
+        } 
+        // If false → print the correct answer
+        else {
             System.out.println("Sorry, the answer is Pong.");
         }
         
@@ -32,13 +30,8 @@ public class Question_2_Quiz_Question {
     
     public static boolean checkAnswer(String answer) {
         
-        // TODO check that the user's answer is "Pong" in any letter case.
-        // Case doesn't matter, so "pong" or "PONG" or any other variation in case is the correct answer.
-        // Remember that using == to compare Strings may not do what you expect it to do!
-        
-        // TODO replace this line with your own code
-        return false;
+     
+        return answer.equalsIgnoreCase("Pong");
     }
     
 }
-

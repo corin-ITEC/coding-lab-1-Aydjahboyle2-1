439fc8eb7efaa1d7af68b4609612cdda8d044656
Corin Dennison <corin.dennison@gmail.com>
2026-05-08 12:46:00 -0500

    Update Question_1_Miles_From_MCTC.java

--- src/main/java/week_1/Question_1_Miles_From_MCTC.java
+++ src/main/java/week_1/Question_1_Miles_From_MCTC.java
@@ -2,42 +2,38 @@ package week_1;
 
 import static input.InputUtils.*;
 
-/**
- * Ask the user how many miles they live from MCTC.
 
- Once the user has typed in their response,
-
- •	Display "You live more than 10 miles from MCTC" if they live more than 10 miles away,
- •	Display "You live exactly 10 miles from MCTC" if they live exactly 10 miles away,
- •	Display "You live less than 10 miles from MCTC" if they live less than 10 miles away.
-
- Use if – else if – else statements.
-
- */
 public class Question_1_Miles_From_MCTC {
 
     public static void main(String[] args) {
 
+        // Ask the user for how many miles they live from MCTC
         double miles = doubleInput("How many miles do you live from MCTC? ");
 
+        // Call the method to get the correct message
         String response = milesFromMCTC(miles);
 
+        // Print the result
         System.out.println(response);
 
     }
 
-    
     public static String milesFromMCTC(double miles){
 
-        // TODO Use if - else if - else statements to return the correct String
+        // If the distance is greater than 10 miles
+        if (miles > 10) {
+            return "You live more than 10 miles from MCTC";
+        } 
+        // If the distance is exactly 10 miles
+        else if (miles == 10) {
+            return "You live exactly 10 miles from MCTC";
+        } 
+        // If the distance is less than 10 miles
+        else {
+            return "You live less than 10 miles from MCTC";
+        }
 
-        // Return  "You live more than 10 miles from MCTC" if they live more than 10 miles away,
-        // Return  "You live exactly 10 miles from MCTC" if they live exactly 10 miles away,
-        // Return  "You live less than 10 miles from MCTC" if they live less than 10 miles away.
-        
-        return null; // TODO delete this line and replace with your code.
     }
-    
-    
+
 }
 

28d950f92d689f68de6343c7139d257c9d170002
Corin Dennison <corin.dennison@gmail.com>
2026-05-08 12:43:36 -0500

    Update Question_6_Windows_10_Install.java

--- src/main/java/week_1/Question_6_Windows_10_Install.java
+++ src/main/java/week_1/Question_6_Windows_10_Install.java
@@ -2,33 +2,24 @@ package week_1;
 
 import static input.InputUtils.*;
 
-/**
- To upgrade to Windows 10, a computer needs to have:
-
- •	Either Windows 7 OR Windows 8 installed   AND
- •	1 GHz or faster processor   AND
- •	2 GB or more RAM
-
- Finish this program that asks the user for the current version of their
- operating system, the CPU speed, and amount of RAM.
-
- Use && and || operator in conditions to finish the method that decides if a user
- is able to install Windows 10.
- 
- Tip: if your program is failing the tests, check page 74 of Think Java, e-book link here,
- https://books.trinket.io/thinkjava2/chapter5.html#sec63, and/or look up short circuit evaluation.
- */
 
 public class Question_6_Windows_10_Install {
 
     public static void main(String[] args) {
 
+        // Ask user for current operating system
         String currentOS = stringInput("What is your current operating system?");
+
+        // Ask for processor speed in GHz
         double processorSpeed = doubleInput("What is your current processor speed, in GHz?");
+
+        // Ask for RAM in GB
         double ram = doubleInput("How much RAM do you have, in GB?");
 
+        // Call method to check if requirements are met
         boolean canUpgradeToWindows10 = checkWindows10SystemRequirements(currentOS, processorSpeed, ram);
 
+        // Print result based on true/false
         if (canUpgradeToWindows10) {
             System.out.println("You can upgrade to Windows 10");
         } else {
@@ -39,14 +30,21 @@ public class Question_6_Windows_10_Install {
 
     public static boolean checkWindows10SystemRequirements(String currentOS, double processorSpeed, double ram) {
 
-        // TODO write code to check if the computer meets the upgrade requirements.
-        // The computer can be upgraded only if ALL the conditions are met.
-        // The two valid currentOS values are the exact Strings "Windows 7" or "Windows 8"
-        // Return true if the computer can be upgraded
-        // Return false if the computer can't be upgraded.
+        // Check ALL requirements at once:
+        // OS must be "Windows 7" OR "Windows 8"
+        // AND processor must be at least 1 GHz
+        // AND RAM must be at least 2 GB
+        if ((currentOS.equals("Windows 7") || currentOS.equals("Windows 8")) 
+                && processorSpeed >= 1 
+                && ram >= 2) {
 
-        // TODO remove this line, and replace with your own code.
-        return false;
+            return true; // meets all requirements
+
+        } else {
+
+            return false; // does not meet requirements
+
+        }
 
     }
     
